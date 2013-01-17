d3c3923dc6f608be4caf8cbc53422db7ebd07313
Seth B. Altshuler <sethb78@gmail.com>
2013-01-16 19:01:45 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoa::Application.config.secret_token = '03e9c8644879526fa4045e23af813e77daf8722692b1c4272c91fcc6a57e2788817a20cbe2d74874af58dc8e11f22f1810dd83507bc3d54ca7afab2e184a3c7f'
