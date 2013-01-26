2f594e7f2e4907ff2ae38ad9178abe100dfa0fbb
Seth B. Altshuler <sethb78@gmail.com>
2013-01-26 12:15:55 -0500

    updated homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,5 @@
 class StaticPagesController < ApplicationController
     before_filter :authenticate_user!, except: [:location, :home, :contact]
  
-  def admin
-  end
-
-
-  def location
-  end
-
-  def home
-  		@documents_rules = Document.where(:doctype => "rules")
-  		@documents_other = Document.where(:doctype => "other")
-  		documents_meeting = Document.where(:doctype => "meetings")
-  		@documents_meeting = documents_meeting.all.sort_by(&:meeting_date).reverse
-  end
 
 end
