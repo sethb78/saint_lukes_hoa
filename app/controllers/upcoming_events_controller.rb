92c12b4593834e07f24e33c1687b03a9dacd7d67
Seth B. Altshuler <sethb78@gmail.com>
2013-01-28 18:49:00 -0500

    update

--- app/controllers/upcoming_events_controller.rb
+++ app/controllers/upcoming_events_controller.rb
@@ -1,7 +1,5 @@
 class UpcomingEventsController < ApplicationController
-  def index
-  @event = UpcomingEvent.find(:all, :conditions => ["event_start between ? and ?", DateTime.now, (DateTime.now + 6.months)])
-  end
+
 
   	def show
   		@upcoming_event = UpcomingEvent.find(params[:id])
