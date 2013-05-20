c410b4b123a36c93c139872e542d21a02dd8a155
Seth B. Altshuler <sethb78@gmail.com>
2013-05-20 19:59:54 -0400

    event date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 before_filter :set_layout_variables
 
 def set_layout_variables
-  @event = UpcomingEvent.find(:all, :conditions => ["event_start between ? and ?", DateTime.now, (DateTime.now + 6.months)])
+  @event = UpcomingEvent.order('event_start ASC').find(:all, :conditions => ["event_start between ? and ?", DateTime.now, (DateTime.now + 6.months)])
 end
 end
