888134db7f0750b8212d93fee97b0786c8c31d0f
sethb78 <sethb78@gmail.com>
2016-10-12 15:37:06 +0000

    st

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hoa::Application.config.secret_token = 17db81ede84391f723da81880c86426edd5941d80c418e10f0a5b4d246d8d129fe7ef3f2aa3b2966e77a2b00292d7354f4070376d97416cdfe152721573e4be5
+Hoa::Application.config.secret_token = '17db81ede84391f723da81880c86426edd5941d80c418e10f0a5b4d246d8d129fe7ef3f2aa3b2966e77a2b00292d7354f4070376d97416cdfe152721573e4be5'
