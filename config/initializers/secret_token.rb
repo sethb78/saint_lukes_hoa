3af5a1021ee1308d6b99f43c6fa88f94f35f79d2
sethb78 <sethb78@gmail.com>
2016-10-12 15:32:09 +0000

    update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,8 +5,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hoa::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
-  ('x' * 30) # meets minimum requirement of 30 chars long
-else
-  config.secret_token=17db81ede84391f723da81880c86426edd5941d80c418e10f0a5b4d246d8d129fe7ef3f2aa3b2966e77a2b00292d7354f4070376d97416cdfe152721573e4be5
-end
+Hoa::Application.config.secret_token = 17db81ede84391f723da81880c86426edd5941d80c418e10f0a5b4d246d8d129fe7ef3f2aa3b2966e77a2b00292d7354f4070376d97416cdfe152721573e4be5
